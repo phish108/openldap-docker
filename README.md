617b84337391c51745ec22ebc6f88a97e4086de9
Christian Glahn <glah@zhaw.ch>
2024-01-01 23:48:48 +0100

    proper support for openldap 2.6.6

--- README.md
+++ README.md
@@ -4,19 +4,16 @@ Docker Container for running tweaked openldap service
 
 ## Installed Schemas
 
-- cosine
-- corba
-- nis
-- inetorgperson
-- ppolicy
-- qmail
+- `corba`
+- `ppolicy`
+- `qmail`
 
-- oidc-client ([OIDC Consortium](https://bitbucket.org/connect2id/server-ldap-schemas))
-- oidc-session ([OIDC Consortium](https://bitbucket.org/connect2id/server-ldap-schemas))
-- oidc-authz ([OIDC Consortium](https://bitbucket.org/connect2id/server-ldap-schemas))
+- `oidc-client` ([OIDC Consortium](https://bitbucket.org/connect2id/server-ldap-schemas))
+- `oidc-session` ([OIDC Consortium](https://bitbucket.org/connect2id/server-ldap-schemas))
+- `oidc-authz` ([OIDC Consortium](https://bitbucket.org/connect2id/server-ldap-schemas))
 
-- eduperson ([REFEDS](https://wiki.refeds.org/display/STAN/eduPerson))
-- swissedu ([Switch EduID](https://help.switch.ch/aai/support/documents/attributes/))
+- `eduperson` ([REFEDS](https://wiki.refeds.org/display/STAN/eduPerson))
+- `swissedu` ([Switch EduID](https://help.switch.ch/aai/support/documents/attributes/))
 
 ## Initial Configuration
 
@@ -39,3 +36,136 @@ All environment variables are needed only for initializing the directory during 
 - `LDAP_ADMIN` (defaults to `admin`): Sets the admin user name. 
 - `LDAP_PASSWORD` (defaults to `secret`): Initializes the admin user password. 
 - `LDAP_DOMAIN` (defaults to `private.domain`): initialises the root to the directory to `dc=private,dc=domain`. 
+
+## Seeding from backups
+
+In order to restore a directory from a backup that has been generated with 
+`slapcat` one has to include the backup files in a special volume. This volumne
+needs to get mouted as `/backup` in the container and must include one or two
+files:
+
+- `backup.ldif` - with the directory backup
+- `config.ldif` - with the configuration backup
+
+## Upgrading a major release 
+
+For a major release one should backup the directory, but NOT the configuration.
+The configuration tends to include breaking changes between the releases and 
+should be recreated from scratch.
+
+When restoring from a backup the Domain, root user (without domain), and the 
+root password need to be provided in addition to the directory backup. This 
+will prepare a fresh configuration. 
+
+Ensure that you place a file with the admin/manager password in a file inside 
+your `backup`-folder.
+
+## Examples
+
+Launch from scratch with defaults
+
+```
+docker run -it --rm -p 389:389 \
+           -e LDAP_DOMAIN=example.com \
+           -v $(pwd)/data:/data \
+           -v $(pwd)/config:/etc/openldap/slapd.d \
+           phish108/openldap:2.6.6-1
+```
+
+Fully recreate from backup.
+
+```
+docker run -it --rm -p 389:389 \
+           -v $(pwd)/backup:/backup \
+           -v $(pwd)/data:/data \
+           -v $(pwd)/config:/etc/openldap/slapd.d \
+           phish108/openldap:2.6.6-1
+```
+
+Upgrade from a previous release.
+
+```
+docker run -it --rm -p 389:389 \
+           -e LDAP_DOMAIN=example.com \
+           -e LDAP_ADMIN=manager \
+           -e LDAP_PASSWORD_FILE=FILENAME_IN_BACKUP \
+           -v $(pwd)/backup:/backup \
+           -v $(pwd)/data:/data \
+           -v $(pwd)/config:/etc/openldap/slapd.d \
+           phish108/openldap:2.6.6-1
+```
+
+Launch normally.
+
+```
+docker run -it --rm -p 389:389 \
+           -v $(pwd)/data:/data \
+           -v $(pwd)/config:/etc/openldap/slapd.d \
+           phish108/openldap:2.6.6-1
+```
+
+Composefile for running normally in a docker swarm.
+
+```YAML
+version: '3.3'
+services:
+  ldapserver:
+    image: phish108/openldap:2.6.6-1
+    networks:
+      - ldapcluster
+    volumes:
+      - "ldapdata:/data"
+      - "ldapconfig:/etc/openldap/slapd.d"
+    ports:
+      - "3890:389"
+
+volumes:
+  ldapdata:
+    external: true
+  ldapconfig:
+    external: true
+
+networks:
+  ldapcluster:
+    external: true
+```
+
+Composefile updating in a docker swarm
+
+```YAML
+version: '3.3'
+services:
+  ldapserver:
+    image: phish108/openldap:2.6.6-1
+    networks:
+      - ldapcluster
+    environment:
+      LDAP_DOMAIN: example.com
+      LDAP_ADMIN: Manager
+      LDAP_PASSWORD_FILE: ldap_password
+    volumes:
+      - "ldapbackup:/backup"
+      - "ldapdata:/data"
+      - "ldapconfig:/etc/openldap/slapd.d"
+    secrets:
+      - source: ldap_password
+        target: /install/ldap_password
+    ports:
+      - "3890:389"
+
+volumes:
+  ldapbackup:
+    external: true
+  ldapdata:
+    external: true
+  ldapconfig:
+    external: true
+
+networks:
+  ldapcluster:
+    external: true
+
+secrets:
+  ldap_password:
+    external: true
+```
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,105 +2,148 @@
 
 ulimit -n 8000
 
-#  /etc/openldap/slapd.d/cn=config | grep mdb > /dev/null
-# if [ "$?" != "0" ]
-#then
-
 # If a fresh container with empty volumes arrives, set up the domain
 if [ ! -f /data/data.mdb ]
 then
-    echo "RootDomain is not configured"
-    echo "init DB"
-
-    if [ -f /install/init.conf ]
+    # check if a backup is present 
+    if [ -f /backup/config.ldif ]
     then
-        echo " ... using init.conf"
-        . /install/init.conf
+        echo "restore config from backup"
+        slapadd -b cn=config -l /backup/config.ldif -F /etc/openldap/slapd.d/
+        echo "config backup restored"
+    else
+        echo "RootDomain is not configured"
+        echo "init DB"
+        # Step 8 of https://www.openldap.org/doc/admin26/quickstart.html
+
+        if [ -f /install/init.conf ]
+        then
+            echo " ... using init.conf"
+            . /install/init.conf
+        fi
+
+        if [ -f /install/password ]
+        then
+            echo " ... using predefined password file"
+            LDAP_PASSWORD=`cat /install/password`
+        fi
+
+        if [ ! -z $LDAP_PASSWORD_FILE ]
+        then
+            if [ -f /install/$LDAP_PASSWORD_FILE ]
+            then
+                echo " ... using password file from install"
+                LDAP_PASSWORD=$(cat /install/$LDAP_PASSWORD_FILE)
+            fi
+            
+            if [ -f /backup/$LDAP_PASSWORD_FILE ]
+            then
+                echo " ... using password file from backup"
+                LDAP_PASSWORD=$(cat $LDAP_PASSWORD_FILE)
+            fi
+        fi
+
+        if [ -z $LDAP_DOMAIN ]
+        then
+            echo " ... using default domain"
+            LDAP_DOMAIN=private.domain
+        fi
+
+        ROOT_DN=`echo $LDAP_DOMAIN | sed -E 's/([^\.]*)/dc=\1/g' | tr . ,`
+
+        # create backend definition
+        # $ROOT_DN
+        # $ROOT_DN_USER
+        if [ -z $LDAP_ADMIN ]
+        then
+            echo " ... using default admin"
+            LDAP_ADMIN=admin
+        fi
+
+        if [ -z $LDAP_PASSWORD ]
+        then
+            echo " ... using default password"
+            LDAP_PASSWORD=secret
+        fi
+
+        # $ROOT_DN_DC (from ROOT_DN)
+        ROOT_DN_DC=`echo $ROOT_DN | sed -E 's/^.*=(.*),.*$/\1/'`
+
+        # $ROOT_DN_ORG (from ROOT_DN (merge all dc=))
+        ROOT_DN_ORG=`echo $ROOT_DN | sed -E 's/[^,]*=([^,]*)/\1/g' | tr "," "."`
+
+        # BACKEND="database mdb\n maxsize 1073741824\n suffix \"$ROOT_DN\"\n rootdn \"cn=$ROOT_DN_USER,$ROOT_DN\"\n rootpw $ROOT_DN_PASSWORD\n directory /var/lib/openldap/openldap-data\n index mail,uid,mailAlias,cn,sn eq,sub\n index objectClass eq"
+
+        LDAP_C_PASSWORD=$(slappasswd -h '{SSHA}' -s "$LDAP_PASSWORD")
+
+        echo "olcSuffix: $ROOT_DN" >> /etc/openldap/slapd.ldif
+        echo "olcRootDN: cn=$LDAP_ADMIN,$ROOT_DN" >> /etc/openldap/slapd.ldif
+        echo "olcRootPW: $LDAP_C_PASSWORD" >> /etc/openldap/slapd.ldif
+
+        # Step 9 of https://www.openldap.org/doc/admin26/quickstart.html
+        slapadd -n 0 -l /etc/openldap/slapd.ldif -F /etc/openldap/slapd.d/
+
+        echo "domain config set"
+
+        if [ ! -f /backup/backup.ldif ]
+        then
+            echo "seed domain"
+            # Step 11 of https://www.openldap.org/doc/admin26/quickstart.html
+
+            echo "
+dn: $ROOT_DN
+objectClass: dcObject
+objectClass: organization
+objectClass: top
+dc: $ROOT_DN_DC
+o: $LDAP_DOMAIN
+
+dn: cn=$LDAP_ADMIN,$ROOT_DN
+objectClass: organizationalRole
+cn: $LDAP_ADMIN
+    " > /install/base.ldif
+
+            slapadd -n 2 -l /install/base.ldif -F /etc/openldap/slapd.d/
+
+            rm /install/base.ldif
+
+            echo "domain seeded" 
+        fi
+
+        echo "init core LDAP schemas" 
+
+        # Initialise all schemas to the latest version
+        # add more core schemas
+        echo "  corba " && slapadd -n 0 -l  /install/schema/corba.ldif
+        echo "  ppolicy " && slapadd -n 0 -l  /install/schema/ppolicy.ldif
+        echo "  qmail " && slapadd -n 0 -l  /install/schema/qmail.ldif
+
+        # EduID Schemas
+        echo "install REFEDS Schemas"
+        echo "  eduperson " && slapadd -n 0 -l  /install/schema/eduperson-202208.openldap.ldif 
+        echo "  swissedu " && slapadd -n 0 -l  /install/schema/swissedu-202208.openldap.ldif  
+
+        # OAuth2 Schemas
+        echo "install OIDC Schemas"
+        echo "  oidc-client " && slapadd -n 0 -l /install/schema/oidc-client-schema-openldap.ldif 
+        echo "  oidc-session " && slapadd -n 0 -l  /install/schema/oidc-session-schema-openldap.ldif
+        echo "  oidc-authz " && slapadd -n 0 -l  /install/schema/oidc-authz-schema-openldap.ldif 
+
+        echo "init DB completed"
     fi
 
-    if [ -f /install/password ]
+    # Check if a directory backup is present 
+    if [ -f /backup/backup.ldif ]
     then
-        echo " ... using password file"
-        LDAP_PASSWORD=`cat /install/password`
+        echo "restore backup"
+        slapadd -b $ROOT_DN -l /backup/backup.ldif -F /etc/openldap/slapd.d/
+        echo "backup restored"
     fi
-
-    if [ -z $LDAP_DOMAIN ]
-    then
-        echo " ... using default domain"
-        LDAP_DOMAIN=private.domain
-    fi
-
-    ROOT_DN=`echo $LDAP_DOMAIN | sed -E 's/([^\.]*)/dc=\1/g' | tr . ,`
-
-    # create backend definition
-    # $ROOT_DN
-    # $ROOT_DN_USER
-    if [ -z $LDAP_ADMIN ]
-    then
-        echo " ... using default admin"
-        LDAP_ADMIN=admin
-    fi
-
-    if [ -z $LDAP_PASSWORD ]
-    then
-        echo " ... using default password"
-        LDAP_PASSWORD=secret
-    fi
-    # ROOT_DN_PASSWORD=`slappasswd -s $ROOT_DN_PASSWORD`
-
-    # $ROOT_DN_DC (from ROOT_DN)
-    ROOT_DN_DC=`echo $ROOT_DN | sed -E 's/^.*=(.*),.*$/\1/'`
-
-    # $ROOT_DN_ORG (from ROOT_DN (merge all dc=))
-    ROOT_DN_ORG=`echo $ROOT_DN | sed -E 's/[^,]*=([^,]*)/\1/g' | tr "," "."`
-
-    # BACKEND="database mdb\n maxsize 1073741824\n suffix \"$ROOT_DN\"\n rootdn \"cn=$ROOT_DN_USER,$ROOT_DN\"\n rootpw $ROOT_DN_PASSWORD\n directory /var/lib/openldap/openldap-data\n index mail,uid,mailAlias,cn,sn eq,sub\n index objectClass eq"
-
-    BACKEND="olcSuffix: $ROOT_DN\\nolcRootDN: cn=$LDAP_ADMIN,$ROOT_DN\\nolcRootPW: $LDAP_PASSWORD"
-    FULLBACKEND=`slapcat -n 0 | sed -e "/olcDatabase:.*mdb/a $BACKEND"`
-
-    ROOTENTRY="dn: $ROOT_DN\\nobjectClass: dcObject\\nobjectClass: organization\\nobjectClass: top\\ndc: $ROOT_DN_DC\\no: $ROOT_DN_ORG\\n\\ndn: cn=$LDAP_ADMIN,$ROOT_DN\\nobjectClass: organizationalRole\\nobjectClass: simpleSecurityObject\\ndescription: LDAP administrator\\ncn: $LDAP_ADMIN\\nuserPassword: $LDAP_PASSWORD\\n"
-
-    # new requirement
-    echo "suffix $ROOT_DN" >> /etc/openldap/slapd.conf
-
-    # Remove the configuration
-    rm -rf /etc/openldap/slapd.d/*
-
-    # Replace the configuration
-    
-    echo "$FULLBACKEND" | slapadd -n 0 -F /etc/openldap/slapd.d
-
-    echo -e $ROOTENTRY | slapadd -n 1
-
-    echo "URI ldapi://%2fvar%2frun%2fopenldap%2fsocket" >> /etc/openldap/ldap.conf
-
-    echo "init DB completed"
 fi
 
-echo "init core LDAP schemas" 
-# Initialise all schemas to the latest version
-echo "  cosine " && slapadd -n 0 -l  /install/schema/cosine.ldif -f /etc/openldap/slapd.conf -F /etc/openldap/slapd.d/
-echo "  corba " && slapadd -n 0 -l  /install/schema/corba.ldif
-echo "  nis " && slapadd -n 0 -l  /install/schema/nis.ldif
-echo "  inetorgperson " && slapadd -n 0 -l  /install/schema/inetorgperson.ldif
-echo "  ppolicy " && slapadd -n 0 -l  /install/schema/ppolicy.ldif
-echo "  qmail " && slapadd -n 0 -l  /install/schema/qmail.ldif
-
-# EduID Schema
-echo "install REFEDS Schemas"
-echo "  eduperson " && slapadd -n 0 -l  /install/schema/eduperson-202208.openldap.ldif 
-echo "  swissedu " && slapadd -n 0 -l  /install/schema/swissedu-202208.openldap.ldif  
-
-# OAuth2 Schema
-echo "install OIDC Schemas"
-echo "  oidc-client " && slapadd -n 0 -l /install/schema/oidc-client-schema-openldap.ldif 
-echo "  oidc-session " && slapadd -n 0 -l  /install/schema/oidc-session-schema-openldap.ldif
-echo "  oidc-authz " && slapadd -n 0 -l  /install/schema/oidc-authz-schema-openldap.ldif 
-
-# allow the ldap user to access the config and data
+# ensure that the ldap user to access the config and data
 chown -R ldap.ldap /etc/openldap/slapd.d
 chown -R ldap.ldap /data
 
+# start the openldap server
 /usr/sbin/slapd -d stats -h "ldap:/// ldapi://%2fvar%2frun%2fopenldap%2fsocket" -u ldap -g ldap
-# exec /bin/ash
